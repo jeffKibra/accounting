b902bc30331652e47bf0ae9153bd25d70fe8e710
jeffKibra <59401150+jeffKibra@users.noreply.github.com>
2022-10-05 19:16:19 +0300

    connected invoice update to firebase functions

--- src/store/sagas/invoices/updateInvoiceSagas.ts
+++ src/store/sagas/invoices/updateInvoiceSagas.ts
@@ -1,10 +1,8 @@
 import { put, call, select, takeLatest } from 'redux-saga/effects';
-import { runTransaction } from 'firebase/firestore';
+import { httpsCallable } from 'firebase/functions';
 import { PayloadAction } from '@reduxjs/toolkit';
 
-import { db } from '../../../utils/firebase';
-import { InvoiceSale } from '../../../utils/invoices';
-import { createDailySummary } from '../../../utils/summaries';
+import { functions } from '../../../utils/firebase';
 
 import { UPDATE_INVOICE } from '../../actions/invoicesActions';
 import { start, success, fail } from '../../slices/invoicesSlice';
@@ -13,13 +11,7 @@ import {
   success as toastSuccess,
 } from '../../slices/toastSlice';
 
-import {
-  RootState,
-  InvoiceFormData,
-  UserProfile,
-  Account,
-  Org,
-} from '../../../types';
+import { RootState, InvoiceFormData, Org } from '../../../types';
 
 interface UpdateData extends InvoiceFormData {
   invoiceId: string;
@@ -28,17 +20,21 @@ interface UpdateData extends InvoiceFormData {
 function* updateInvoiceSaga(action: PayloadAction<UpdateData>) {
   yield put(start(UPDATE_INVOICE));
   const org: Org = yield select((state: RootState) => state.orgsReducer.org);
-  const userProfile: UserProfile = yield select(
-    (state: RootState) => state.authReducer.userProfile
-  );
-  const accounts: Account[] = yield select(
-    (state: RootState) => state.accountsReducer.accounts
-  );
+  const { orgId } = org;
+
+  const {
+    payload: { invoiceId, ...formData },
+  } = action;
 
   async function update() {
-    /**
-     * itialize by creating the daily summary if none is available
-     */
+    return httpsCallable(
+      functions,
+      'sales-invoice-update'
+    )({
+      orgId,
+      invoiceId,
+      formData,
+    });
   }
 
   try {
